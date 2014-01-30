9f378d551325cd9942348e894abcf4219df4549a
meghshyam <meghshyam@gmail.com>
2014-01-30 14:54:53 +0800

    changed clustering code

--- DetectFiducial/src/main.cpp
+++ DetectFiducial/src/main.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 void findConnectedComponents(Mat &, Mat & );
 void doCluster(Mat&, Mat&);
+float distL2(int * a, float *b, int dim);
 int main(int argc, char* argv[])
 {
 	if(argc < 2){
@@ -34,16 +35,27 @@ int main(int argc, char* argv[])
 	GaborFilter gbFilter;
 	Mat gaborOutput(input.size(), CV_8UC1);
 	gbFilter.filter(input, gaborOutput);
-	imshow("gabour output", gaborOutput);
+
 	//Find Connected components in Gabor output
 	Mat connectedComponents, clusters;
 	findConnectedComponents(gaborOutput, connectedComponents);
 
 	//Cluster the connected components
 	doCluster(connectedComponents, clusters);
+	int numClusters = clusters.rows;
+	for(int i=0; i<numClusters; i++)
+	{
+		int * row = (int *)&clusters.at<int>(i,0);
+		int x1 = row[0];
+		int y1 = row[1];
+		int x2 = row[2];
+		int y2 = row[3];
+		Point pt1(x1,y1), pt2(x2,y2);
+		rectangle(input, pt1, pt2, Scalar(0,255,0) );
+	}
 
-	//Merge the clusters
-
+	imshow("cluster output", input);
+	waitKey();
 	//Detect code in the bounding box
 
 	//Classify the detected code
@@ -67,14 +79,12 @@ void findConnectedComponents(Mat &binaryImage, Mat &out){
 			int y1 = stats.at<int>(i, CC_STAT_TOP);
 			int x2 = x1 + stats.at<int>(i, CC_STAT_WIDTH);
 			int y2 = y1 + stats.at<int>(i, CC_STAT_HEIGHT);
-			Point pt1(x1,y1), pt2(x2,y2);
+			//Point pt1(x1,y1), pt2(x2,y2);
 			int * row = (int *)&out.at<int>(i-1,0);
 			row[0] = x1; row[1] = y1; row[2] = x2; row[3] = y2;
-			rectangle(binaryImage, pt1, pt2, Scalar(255,0,0) );
+			//rectangle(binaryImage, pt1, pt2, Scalar(255,0,0) );
 		}
 	}
-	imshow( "Connected Components", binaryImage);
-	waitKey();
 }
 
 void doCluster(Mat &connectedComponents, Mat& clusters)
@@ -86,4 +96,57 @@ void doCluster(Mat &connectedComponents, Mat& clusters)
 	// since you get less clusters than you specified we can also truncate our matrix.
 	centers = centers.rowRange(cv::Range(0,true_number_clusters));
 
+	int *clusterId = new int[true_number_clusters];
+	int *firstMember = new int[true_number_clusters];
+	for(int i=0; i<true_number_clusters; i++)
+	{
+		firstMember[i] = 0;
+	}
+	clusters.create(true_number_clusters, 4, DataType<int>::type);
+
+	for(int i=0; i<numElements; i++)
+	{
+		int * row = (int *)&connectedComponents.at<int>(i,0);
+		float * centre = (float *)&centers.at<int>(0,0);
+		int minIndex = 0;
+		float minValue = distL2(row, centre, 4);
+		for(int j=1; j<true_number_clusters; j++){
+			centre = (float *)&centers.at<int>(j,0);
+			float dist = distL2(row, centre, 4);
+			if(dist < minValue)
+			{
+				minValue = dist;
+				minIndex = j;
+			}
+		}
+		clusterId[i] = minIndex;
+		int *cluster= (int*) &clusters.at<int>(minIndex,0);
+		if(firstMember[minIndex] == 0)
+		{
+			firstMember[minIndex] = 1;
+			cluster[0]= row[0];
+			cluster[1]= row[1];
+			cluster[2]= row[2];
+			cluster[3]= row[3];
+		}
+		else{
+			if(cluster[0] > row[0])
+				cluster[0] = row[0];
+			if(cluster[1] > row[1])
+				cluster[1] = row[1];
+			if(cluster[2] < row[2])
+				cluster[2] = row[2];
+			if(cluster[3] < row[3])
+				cluster[3] = row[3];
+		}
+	}
+}
+
+float distL2(int * a, float *b, int dim)
+{
+	float distance=0;
+	for(int i=0; i<dim; i++){
+		distance += (a[i] - b[i])*(a[i] - b[i]);
+	}
+	return sqrt(distance);
 }
